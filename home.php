951749eae0c77efe52b6a81d6b38158ae46c0775
arthurtherunner <arthurmoura904@gmail.com>
2025-11-10 17:27:50 -0300

    Começando a codar o home

--- home.php
+++ home.php
@@ -1,3 +1,55 @@
-<head><title>Home</title></head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.0/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="styles/home.css">
+</head>
+<body>
+    <div class="container">
+        <div class="left">
+            <!--add a mensages de boas vindas e o icon-->
+            <div class="paragrafos">
+                <p>Início</p>
+                <hr>
+                <p>Receitas favoritas</p>
+                <hr>
+                <p>Sair</p>
+                <hr>
+            </div>
+        </div>
+            <div class="right">
+                <div class="receitas">
+                    <img src="assets/BOLO DE CHOCOLATE.png" alt="">
+                    <div class="desc"><p>Bolo de chocolate</p>
+                    <i class="bi bi-heart-fill"></i>
+                    </div>
+                </div>
 
-<h1>Olá!</h1>
+                <div class="receitas">
+                    <img src="assets/LASANHA.png" alt="">
+                    <div class="desc"><p>Lasanha</p>
+                    <i class="bi bi-heart-fill"></i>
+                    </div>
+                </div>
+                
+                <div class="receitas">
+                    <img src="assets/ACARAJIVIS.png" alt="">
+                    <div class="desc"><p>Massa de acarajé</p>
+                    <i class="bi bi-heart-fill"></i>
+                    </div>
+                </div>
+
+                <div class="receitas">
+                    <img src="assets/FRICASSE.png" alt="">
+                    <div class="desc"><p>Fricassé</p>
+                    <i class="bi bi-heart-fill"></i>
+                    </div>
+                </div>
+            </div>
+
+    </div>
+</body>
+</html>
