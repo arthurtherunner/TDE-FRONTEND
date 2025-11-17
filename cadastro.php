0220f92f2b2b1f8dcc5c39e6ff05bf02e0cbc5f2
arthurtherunner <arthurmoura904@gmail.com>
2025-11-17 13:44:15 -0300

    Responsividade da tela de cadastro

--- cadastro.php
+++ cadastro.php
@@ -21,7 +21,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
     <link rel="stylesheet" href="styles/style.css">
-    <link rel="stylesheet" href="styles/responsive.css">
 </head>
 <body>
     <div class="container">
